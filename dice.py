a6b42a74b04bf94a7df30f82db055a2aa44457c7
Willow R <wreinitz@eagles.bridgewater.edu>
2025-10-13 14:31:20 -0400

    fixed error from importing

--- dice.py
+++ dice.py
@@ -1,4 +1,4 @@
-import randrange from random
+from random import randrange
 
 
 
@@ -7,5 +7,9 @@ def d(i): # i for input
     for n in range(1, i+1):
         o_list.append(n)
     return o_list
+
+def d_combiner(count, d_name): # count for dice qnty, d_name for dice type
+    pass
+    
 
 print(d(6))
