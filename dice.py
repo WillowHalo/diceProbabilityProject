7010342e2a7fbaff5d56cc086b1c03a43a604a7f
Willow R <wreinitz@eagles.bridgewater.edu>
2025-11-03 15:30:09 -0500

    finalize code of XdY more comments may come

--- dice.py
+++ dice.py
@@ -15,37 +15,33 @@ def XdY(X, dY, r_lim=7776, print_spam=False): # count for dice qnty, d_name for 
     max_dice = []
     ### vars
 
-    for i in range(X):
+    for i in range(X): # set up dice lists
         dice.append(dY[0])
         max_dice.append(dY[-1])
 
     run = 0
     while run < r_lim or r_lim == "inf":
-        if print_spam == True:
+        if print_spam == True: # to watch creation of list step by step
             print(dice)
         
         o_list.append([]) # ready new point
-
+        
         for n in range(X):
-            o_list[run].append(dice[n]) # add values to new point
-
+            o_list[run].append(dice[n]) # add values to new point           Neccisary beecause directly adding lists uses deeper memory
+        
         n = 0
         for d in dice:
-            n += 1
-
             if max_dice == dice:
-                return o_list
-
+                return o_list # return if finnished
             if d != dY[-1]:
-                dice[n-1] += 1
-                break
+                dice[n] += 1
+                break # add to first unfull, then stop
             else:
-                dice[n-1] = dY[0]
-            
+                dice[n] = dY[0] # if max, roll over
+            n += 1
         run += 1
     print("Runtime Error; runtime in excess of limit ("+str(run)+")")
-    return o_list
+    return o_list # if while loop didnt return results
 
     
 
-print( XdY(16, d(4)) )
