fd990563992e605f5c32d5fd169b9b7c5a3f27e8
Willow R <wreinitz@eagles.bridgewater.edu>
2025-11-03 14:51:51 -0500

    added the XdY function to allow for created all dice combo of named purpose

--- dice.py
+++ dice.py
@@ -8,8 +8,44 @@ def d(i): # i for input
         o_list.append(n)
     return o_list
 
-def d_combiner(count, d_name): # count for dice qnty, d_name for dice type
-    pass
+def XdY(X, dY, r_lim=7776, print_spam=False): # count for dice qnty, d_name for dice type
+    ### vars
+    o_list = []
+    dice = []
+    max_dice = []
+    ### vars
+
+    for i in range(X):
+        dice.append(dY[0])
+        max_dice.append(dY[-1])
+
+    run = 0
+    while run < r_lim or r_lim == "inf":
+        if print_spam == True:
+            print(dice)
+        
+        o_list.append([]) # ready new point
+
+        for n in range(X):
+            o_list[run].append(dice[n]) # add values to new point
+
+        n = 0
+        for d in dice:
+            n += 1
+
+            if max_dice == dice:
+                return o_list
+
+            if d != dY[-1]:
+                dice[n-1] += 1
+                break
+            else:
+                dice[n-1] = dY[0]
+            
+        run += 1
+    print("Runtime Error; runtime in excess of limit ("+str(run)+")")
+    return o_list
+
     
 
-print(d(6))
+print( XdY(16, d(4)) )
